28c2e21aea4734c28292940728cfaf2db453e5d0
Vishnu <228a1a4440@risekrishnasaiprakasam.edu.in>
2026-06-24 23:33:27 +0530

    update project

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import { AuthProvider } from "./context/AuthContext";
 import { ThemeProvider } from "./context/ThemeContext";
 
+const Router = import.meta.env.BASE_URL === "/" ? BrowserRouter : HashRouter;
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
       <AuthProvider>
-        <BrowserRouter>
+        <Router>
           <App />
-        </BrowserRouter>
+        </Router>
       </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
-
